aa04936ac8fd894f00779ea3f80909dbba4af5c4
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-04-19 09:49:11 -0300

    feat: icons

--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="bg-gray-800 flex text-white max-h-screen h-screen">
-        <div class="border w-64">
-            <span class="font-semibold">skateboard</span>
-            <nav>
-                <h3>Menu</h3>
-                <div>
-                    <div class="bg-orange-500">
-                        icon
-                    </div>
-                    <span>Discover</span>
+        <div class="w-64">
+            <span class="font-semibold p-6 block opacity-90">skateboard</span>
+            <nav class="p-6">
+                <h3 class="opacity-50 uppercase font-semibold tracking-wiider mb-6">Menu</h3>
+               
+                <div class="space-y-6">
+                    <MenuItem :icon="HomeIcon">Discover</MenuItem>
+                    <MenuItem :icon="TrendingUpIcon">Trending</MenuItem>
+                    <MenuItem :icon="TrendingUpIcon">Trending</MenuItem>
+                    <MenuItem :icon="TrendingUpIcon">Trending</MenuItem>
+                    <MenuItem :icon="TrendingUpIcon">Trending</MenuItem>
                 </div>
+
             </nav>
         </div>
 
@@ -18,3 +21,9 @@
         </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+    import MenuItem from './navs/MenuItem.vue';
+    import TrendingUpIcon from '../components/icons/TrendingUpIcon.vue';
+    import HomeIcon from '../components/icons/HomeIcon.vue';
+</script>
---
+++ src/layouts/navs/MenuItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-center space-x-4">
+        <div class="bg-orange-500 p-2 rounded-lg">
+            <component :is="icon" class="w-4 h-4" />
+        </div>
+        <span><slot/></span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    defineProps({
+        icon: { default: null }
+    })
+</script>
