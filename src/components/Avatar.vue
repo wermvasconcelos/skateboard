931cd4652c60ba91c26e2b3ce6648afbeae8b5ef
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-04-23 14:08:13 -0300

    feat: avatar

---
+++ src/components/Avatar.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="https://via.placeholder.com/400" class="object-cover">
+</template>
--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -4,7 +4,7 @@
             <span class="font-semibold p-6 block opacity-90">skateboard</span>
             <nav class="p-6">
                 <h3 class="opacity-50 uppercase font-semibold tracking-wiider mb-6">Menu</h3>
-               
+
                 <div class="space-y-6">
                     <MenuItem :icon="HomeIcon">Discover</MenuItem>
                     <MenuItem :icon="TrendingUpIcon">Trending</MenuItem>
@@ -16,23 +16,30 @@
             </nav>
         </div>
 
+
         <div class="p-5 flex-1">
-            <header>
-                <InputSearch/>
+            <header class="flex items-center justify-between">
+                <InputSearch />
+                <div>
+                    <div class="flex items-center space-x-3">
+                        <div class="w-10 h-10">
+                            <Avatar class="rounded-full" />
+                        </div>
+                        <span>Wermenson</span>
+                        <ChevronDownIcon class="w-4 h-4" />
+                    </div>
 
-                <!-- <Dropdown>
-                    <Avatar/>
-                </Dropdown>
-
-                <Notifications/> -->
+                    <Notifications />
+                </div>
             </header>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-    import MenuItem from './navs/MenuItem.vue';
-    import TrendingUpIcon from '../components/icons/TrendingUpIcon.vue';
-    import HomeIcon from '../components/icons/HomeIcon.vue';
-    import InputSearch from '../components/inputs/InputSearch.vue';
+import MenuItem from './navs/MenuItem.vue';
+import TrendingUpIcon from '../components/icons/TrendingUpIcon.vue';
+import HomeIcon from '../components/icons/HomeIcon.vue';
+import InputSearch from '../components/inputs/InputSearch.vue';
+import Avatar from '../components/Avatar.vue';
 </script>
