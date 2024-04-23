85a95525edbfe4acc401d95e60625b0987ca7e43
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-04-23 16:02:31 -0300

    feat: discover

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,3 +1,5 @@
 <template>
-    <img src="https://via.placeholder.com/400" class="object-cover">
+    <div class="flex">
+        <img src="https://via.placeholder.com/800x400" class="object-cover">
+    </div>
 </template>
--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="bg-gray-800 flex text-white max-h-screen h-screen">
+        <!-- Side bar -->
         <div class="w-64">
             <span class="font-semibold p-6 block opacity-90">skateboard</span>
             <nav class="p-6">
@@ -16,23 +17,57 @@
             </nav>
         </div>
 
-
+        <!-- Searc e avatar + notificação -->
         <div class="p-5 flex-1">
             <header class="flex items-center justify-between">
-                <InputSearch />
-                <div>
+                <InputSearch class="w-96"/>
+                <div class="flex items-center">
                     <div class="flex items-center space-x-3">
-                        <div class="w-10 h-10">
-                            <Avatar class="rounded-full" />
-                        </div>
+                        <Avatar class="rounded-full overflow-hidden w-10 h-10" />
                         <span>Wermenson</span>
                         <ChevronDownIcon class="w-4 h-4" />
                     </div>
 
-                    <Notifications />
+                    <BellIcon class="w-5 h-5 ml-8 opacity-50 hover:opacity-100 cursor-pointer transition durantion-100"/>
                 </div>
             </header>
+
+            <h1 class="font-bold text-4xl mt-6 mb-4">Discover</h1>
+            
+            <div class="grid gap-4 grid-cols-12">
+
+                <div class="col-span-8 h-96 rounded-3xl borde overflow-hidden relative">
+                    <div class="flex w-full h-full">
+                        <img src="https://via.placeholder.com/2000x1400" class="w-full object-cover">
+                    </div>
+
+                    <div class="z-10 absolute top-0 p-6 w-full bottom-0">
+                        <h3 class="text-2xl max-w-xs">How to do Basic Jumping and how to landing safely</h3>
+
+                        <div class="absolute right-6 bottom-6 px-2 py-1 rounded-lg bg-gray-800 bg-opacity-50 text-white">7 minutes</div>
+                    </div>
+                </div>
+
+                <div class="col-span-4 h-96 rounded-3xl borde overflow-hidden relative">
+                    <div class="flex w-full h-full">
+                        <img src="https://via.placeholder.com/2000x1400" class="w-full object-cover">
+                    </div>
+
+                    <div class="z-10 absolute top-0 p-6 w-full bottom-0">
+                        <h3 class="text-2xl max-w-xs">How to do Basic Jumping and how to landing safely</h3>
+
+                        <div class="absolute right-6 bottom-6 px-2 py-1 rounded-lg bg-gray-800 bg-opacity-50 text-white">7 minutes</div>
+                    </div>
+                </div>
+
+                
+            </div>
+
+            
+
         </div>
+
+
     </div>
 </template>
 
@@ -42,4 +77,6 @@ import TrendingUpIcon from '../components/icons/TrendingUpIcon.vue';
 import HomeIcon from '../components/icons/HomeIcon.vue';
 import InputSearch from '../components/inputs/InputSearch.vue';
 import Avatar from '../components/Avatar.vue';
+import ChevronDownIcon from '../components/icons/ChevronDownIcon.vue';
+import BellIcon from '../components/icons/BellIcon.vue';
 </script>
